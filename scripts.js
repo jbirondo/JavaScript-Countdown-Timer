f92cc9ad45e1a9cc6aada66cf58421c707e3fe92
Eugene Birondo <jbirondo@gmail.com>
2020-06-11 22:26:57 -0700

    Working on displayEndTime

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
 
 function timer(seconds) {
     const now = Date.now()
     const then = now + seconds * 1000
     displayTimeLeft(seconds)
+    displayEndTime(then)
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000)
         if(secondsLeft < 0) {
@@ -21,4 +23,11 @@ function displayTimeLeft(seconds){
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
     timerDisplay.textContent = display
     document.title = display
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp)
+    const hours = end.getHours()
+    const minutes = end.getMinutes()
+    endTime.textContent = `Be back at ${hours > 12 ? hours - 12 : hours}:${minutes < 10 ? '0' : ''}${minutes}`
 }
