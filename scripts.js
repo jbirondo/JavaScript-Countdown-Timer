9e850874e09996eec29cea048484e4460b23cec1
Eugene Birondo <jbirondo@gmail.com>
2020-06-11 21:56:24 -0700

    Working on timer

--- scripts.js
+++ scripts.js
@@ -3,10 +3,11 @@ let countdown
 function timer(seconds) {
     const now = Date.now()
     const then = now + seconds * 1000
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000)
-        if(secondsLeft <= 0) {
-
+        if(secondsLeft < 0) {
+            clearInterval(countdown)
+            return
         }
     }, 1000);
 }
